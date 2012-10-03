e10d907c1b9c3bb2c692c8450c39d01920fa8457
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-10-03 12:09:36 +0800

    start of something new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindext::Application.config.secret_token = '1831ab573cc101951f76f105a1c56aab5fbbd6e30beed4bb4a44fd07c6fa4637eab2937b0714184279cb3757142ecd391f9ff5b63b0f095b51e15a3c7d601eab'
