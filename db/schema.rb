8978bd9314b66185143f14ff54e6c7be36bc8b01
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-10-08 10:46:10 +0800

    Created tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006132102) do
+ActiveRecord::Schema.define(:version => 20121008024518) do
 
   create_table "compositions", :force => true do |t|
     t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "tag"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
